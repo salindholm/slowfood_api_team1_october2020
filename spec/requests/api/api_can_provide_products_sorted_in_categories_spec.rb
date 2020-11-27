d64cd7774d660d02b1785b684c91cb6b0834a476
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-27 15:52:50 +0100

    Add more tests to API can provide products in categories in request spec

--- spec/requests/api/api_can_provide_products_sorted_in_categories_spec.rb
+++ spec/requests/api/api_can_provide_products_sorted_in_categories_spec.rb
@@ -8,12 +8,20 @@ RSpec.describe "GET/api/products" do
       get "/api/categories/#{category.title}"
     end
 
-    it "expected to return a 200 response" do
+    it "is expected to return a 200 response" do
       expect(response).to have_http_status 200
     end
   
-    it "expected to return all categories" do
+    it "is expected to return all categories" do
       expect(response_json["category"]["products"].count).to eq 2
+    end
+
+    it "is expected to return the title in category" do
+      expect(response_json["category"]["title"]).to eq "veggie"
+    end
+
+    it "is expected to show title of one of the products" do
+      expect(response_json['category']['products'][1]['title']).to eq 'MyString'
     end
   end
 end
