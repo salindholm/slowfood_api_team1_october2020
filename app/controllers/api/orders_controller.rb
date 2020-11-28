349b0ccc6e06faef22874059ad09defadbec9d30
Sanne <sannelindholm@hotmail.com>
2020-11-28 22:27:07 +0100

    Creating test for user to add second product to order, creating an update method in orders_controller.rb and adding update route to orders.

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -13,4 +13,17 @@ class Api::OrdersController < ApplicationController
     render json: { message: 'Something went wrong...' }, status: 422
    end
   end
+
+  def update
+    order = Order.find(params[:id])
+    product = Product.find(params[:product_id])
+    if new_item = order.items.create(product: product)
+    render json: { message:'Product was successfully added to your order',
+      order: order,
+      items: order.product
+      }, status: 201
+     else 
+      render json: { message: 'Something went wrong...' }, status: 422
+     end
+  end
 end
---
+++ spec/requests/api/add_second_product_to_an_order_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe 'PUT /api/orders/:id', type: :request do
+  let!(:registered_user) { create(:user)}
+  let!(:authorization_headers) { registered_user.create_new_auth_token}
+  let!(:existing_order) { create(:order, user: registered_user) }
+  let!(:product_already_in_order) { create(:product)}
+  let!(:product_to_add) { create(:product, title: 'Vesuvio')}
+
+  describe 'with a valid product id' do
+    before do
+      existing_order.items.create(product: product_already_in_order)
+      put "/api/orders/#{existing_order.id}",
+        params: { product_id: product_to_add.id },
+        headers: authorization_headers
+    end
+
+    it {
+    
+      expect(response).to have_http_status 201
+    }
+
+    it 'is expected to have product data in "item"' do
+      expect(JSON.parse(response.body)['items'][1]['title']).to eq "Vesuvio"
+    end
+  end
+end
