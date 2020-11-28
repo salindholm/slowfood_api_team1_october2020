9ce077d63fef283481db93628d441aede5f5d499
Sanne <sannelindholm@hotmail.com>
2020-11-28 13:45:33 +0100

    Adding sad path with incorrect password confirmation to test

--- spec/requests/api/user_registration_endpoint_spec.rb
+++ spec/requests/api/user_registration_endpoint_spec.rb
@@ -17,4 +17,30 @@ RSpec.describe 'POST /api/auth/', type: :request do
       expect(JSON.parse(response.body)['status']).to eq 'success'
     end
   end
+
+  describe 'with invalid credentials' do
+    before do
+      post '/api/auth/',
+        params: {
+          email: 'user@mail.com',
+          password: 'password',
+          password_confirmation: 'wrong_password' 
+        }
+    end
+    it {
+      expect(response).to have_http_status 422
+    }
+
+    it 'is expected to respond with a "status" message' do
+      expect(JSON.parse(response.body)['status']).to eq 'error'
+    end
+
+    it 'is expected to respond with an error message regarding password confirmation' do
+      expect(
+        JSON.parse(response.body)['errors']['full_messages']
+      )
+      .to include "Password confirmation doesn't match Password"
+    end
+
+  end
 end
