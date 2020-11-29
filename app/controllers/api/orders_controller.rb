dd735e58c1bc7a7e24dd0b573c0c74852429044b
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-29 21:24:24 +0100

    Fix Add to order method

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,13 +1,13 @@
 class Api::OrdersController < ApplicationController
   before_action :authenticate_user!
   def create
-   product = Product.find_by(id: params[:product_id])
+  product = Product.find_by(id: params[:product_id])
    order = current_user.orders.create
    order.items.create(product: product)
    if order.persisted?
+    
     render json: { message:'Product was successfully added to your order',
-    order: order,
-    items: order.product
+    order: order_as_json(order),
     }, status: 201
    else 
     render json: { message: 'Something went wrong...' }, status: 422
@@ -16,14 +16,22 @@ class Api::OrdersController < ApplicationController
 
   def update
     order = Order.find(params[:id])
-    product = Product.find(params[:product_id])
+     product = Product.find(params[:product_id])
     if new_item = order.items.create(product: product)
     render json: { message:'Product was successfully added to your order',
-      order: order,
-      items: order.product
+      order: order_as_json(order),
       }, status: 201
      else 
       render json: { message: 'Something went wrong...' }, status: 422
      end
+  end
+
+  private
+
+
+  def order_as_json(order)
+    order_as_json = order.as_json
+    order_as_json['items'] = order.product.as_json
+    order_as_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+meat = Category.create({ title: 'Meat' })
+veggie = Category.create({ title: 'Veggie' })
+glutenfree = Category.create({ title: 'GlutenFree' })
+Product.create([
+  {
+    title: "Hawaii",
+    ingredients: "tomato sauce, cheese, ham and pineapple",
+    price: 179,
+    category: meat
+  },
+  {
+    title: "Margherita",
+    ingredients: "tomato sauce and cheese",
+    price: 129,
+    category: veggie
+  },
+  {
+    title: "Vesuvio",
+    ingredients: "tomato sauce, cheese and ham",
+    price: 139,
+    category: glutenfree
+  }
+])
