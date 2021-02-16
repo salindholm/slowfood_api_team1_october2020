e5f593796f1b6e4b0e8d8429ce773da1837c50a9
Sanne <sannelindholm@hotmail.com>
2021-02-16 13:08:30 +0100

    Creating product model Creating product controller Creating db and migrating

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'products/index'
   namespace :api do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
--- spec/requests/api/visitor_can_see_list_of_products.spec.rb
+++ spec/requests/api/visitor_can_see_list_of_products.spec.rb
@@ -1,15 +1,16 @@
 RSpec.describe 'GET /api/products', type: :request do
-  let!(:product) 3.times {create(:product)}
+  # let!(:product) {3.times {create(:product)}}
   describe 'successfully get a list of posts' do
     before do
-      get '/api/products',
+      get '/api/products'
     end
 
-    it 'is expected to return a 200 status' do
-      expect(response).to have_http_status 200 
+    it 'is expected to return a 200 response' do
+      expect(response).to have_http_status 200
     end
 
-    it 'is expected to return a list of products'
-    expect(reponse_json['products'].count).to eq 3
+    it 'is expected to return a list of products' do
+      expect(response_json['products'].count).to eq 3
+    end
   end
 end
