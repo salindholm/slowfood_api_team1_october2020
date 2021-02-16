48726c6301112f5ffd024ca305ec696040567461
Sanne <sannelindholm@hotmail.com>
2021-02-16 19:16:47 +0100

    Creating facroty bot and model spec for user model Configuring devise token auth setup Migrating

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe User, type: :model do
+  describe 'Database table' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :tokens }
+  end
+  describe 'Factory' do
+    it 'is expected to have valid factory bot' do
+      expect(create(:user)).to be_valid
+    end
+  end
+end
