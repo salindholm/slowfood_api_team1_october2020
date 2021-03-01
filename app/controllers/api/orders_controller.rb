b111b8be391a294cb44a3418419c041b805ffa9d
Sanne <sannelindholm@hotmail.com>
2021-03-01 14:09:19 +0100

    Adding order model spec and modifying user model spec Modifying routes and test

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,5 @@
 class Api::OrdersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
 
   def create
     product = Product.find(params[:product_id])
