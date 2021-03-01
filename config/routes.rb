fcd5d0897bc0cb6046b971110ea6c8c272fe1914
Sanne <sannelindholm@hotmail.com>
2021-03-01 14:02:17 +0100

    Generating order model Generating orders controller Creating associations between order and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'orders/create'
+  end
   mount_devise_token_auth_for 'User', at: 'api/auth'
   get 'products/index'
   namespace :api do
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_174319) do
+ActiveRecord::Schema.define(version: 2021_03_01_125848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -48,4 +55,5 @@ ActiveRecord::Schema.define(version: 2021_02_16_174319) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
