a3b00dfe55bc70b931a34c55014acd44a5e9e77d
Sanne <sannelindholm@hotmail.com>
2021-03-10 16:26:15 +0100

    Adding update route and method

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -2,8 +2,24 @@ class Api::OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    product = Product.find(params[:product_id])
     order = current_user.orders.create
-    order.order_items.create(product_id: params[:product_id])
-    render json: { message: 'The product has successfully been added to your order!', items: order.order_items }
+    order.order_items.create(product: product)
+    if order.persisted?
+      render json: { message: 'The product has successfully been added to your order!', items: order.order_items }, status: 201
+    else
+      render json: { message: 'Ops, something went wrong.' }, status: 422
+    end
+  end
+
+  def update
+    order = Order.find(params[:id])
+    product = Product.find(params[:product_id])
+    new_item = order.order_items.create(product: product)
+    if new_item.persisted?
+      render json: { message: 'The product has successfully been added to your order!', items: order.order_items }, status: 201
+    else
+      render json: { message: 'Ops, something went wrong.' }, status: 422
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     resources :products, only: [:index]
-    resources :orders, only: [:create]
+    resources :orders, only: %i[create update]
   end
 end
